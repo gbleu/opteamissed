478a85b1da42cb1aa5998e9e3582632d8216cff7
bleug <gabriel.bleu@altares.com>
2017-10-10 12:05:51 +0200

    add placeholder

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ A chrome extension to fix opteamis.com broken UX
 Clone the repo and follow instructions [here](https://developer.chrome.com/extensions/getstarted#unpacked)
 
 ## features
+
 - replace selects by [selectize.js](https://selectize.github.io/selectize.js/)
 - wider modal
+
 ## TODO
+
 - presets
--- opteamissed.js
+++ opteamissed.js
@@ -1,6 +1,22 @@
 function selectize() {
   console.log('selectize');
-  $('select').selectize();
+  $('select')
+    .not('.selectized')
+    .each(function(i, it) {
+      const prevVal = $(it)
+        .find('option[selected]')
+        .val();
+      const optLength = $(it).find('option').length;
+      const firstVal = $(it)
+        .find('option:first')
+        .val();
+
+      $(it).prepend(`<option value=""></option>`);
+      const val = prevVal || (optLength === 1 ? firstVal : '');
+      $(it).val(val);
+
+      $(it).selectize({ placeholder: 'Choose one...' });
+    });
 }
 
 $(document).ajaxComplete(function() {
